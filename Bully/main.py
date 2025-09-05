f82dddbb6983e9243e9f283738c9705ef89802f6
David O^O <78424061+RancidMarmalade@users.noreply.github.com>
2025-09-05 12:13:44 +0200

    Update main.py
    hejsa

--- Bully/main.py
+++ Bully/main.py
@@ -1,22 +1,30 @@
 
-class state(str)
-
+class State(str):
+    pass
 
 
 class Node:
-    def __init__(self, id):
+    def __init__(self, id, state):
         self.id = id
-        self.sstate = state
+        self.state = state
+
+    def set_state(self, state):
+        self.state = state
     
+    def get_state(self):
+        return self.state
+
     def send_message(self, message, destination):
-        pass
+        if(message = "ping"):
+            
 
 class Network:
-    def __init__(self,nodes,leader):
+    def __init__(self, nodes, leader):
         self.nodes = nodes
         self.leader = leader
-    def update_leader(self,leaderId):
-        sel
+
+    def update_leader(self, leaderId):
+        self.leader = leaderId
 
 
 def main():
